ee8c8e467d3e468e929f846fe5315558cc258820
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2023-10-12 18:14:48 +0200

    fix import

--- src/lib/core/content-manager/index.ts
+++ src/lib/core/content-manager/index.ts
@@ -1,5 +1,5 @@
 import { Content } from './content.js';
-import type { ContentManager } from './contentManager.js';
+import { ContentManager } from './contentManager.js';
 import { Filesystem, JsonFilesystem } from './modules/filesystem.js';
 
 export { Content, Filesystem, JsonFilesystem, ContentManager };
