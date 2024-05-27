65859920db6acf0aebd2752d591fc47515e759af
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-05-27 23:14:57 +0200

    fix component

--- packages/create-embodi/templates/skeleton/__layout/Home.svelte
+++ packages/create-embodi/templates/skeleton/__layout/Home.svelte
@@ -1,12 +1,9 @@
 <script>
-	import Counter from "./components/Counter.svelte";
-
 	export let data;
 	export let content;
 </script>
 
 <main>
 	<h1>{data.title}</h1>
-	<Counter />
 	<p>{@html content}</p>
 </main>
