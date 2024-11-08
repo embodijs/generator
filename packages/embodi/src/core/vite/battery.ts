54888052351327d6c9cb5d4f7d6c5a353039d6f1
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-11-09 00:39:12 +0100

    remove type in import to include ts files

--- packages/embodi/src/core/vite/battery.ts
+++ packages/embodi/src/core/vite/battery.ts
@@ -15,7 +15,7 @@ export function embodiBattery() {
 			if (id.endsWith('.battery')) {
 				return `import { laod } from '/${id.slice(0, -7)}';`;
 			} else if (id.endsWith('.js.embodi') || id.endsWith('.ts.embodi')) {
-				return `export * from '/${id.slice(2, -10)}.js';`;
+				return `export * from '/${id.slice(2, -10)}';`;
 			}
 		}
 	} satisfies Plugin;
