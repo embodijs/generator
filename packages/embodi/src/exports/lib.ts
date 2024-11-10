6ce7fc9696073a43a69deb0a5fff2f2134e702f1
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-11-10 22:29:12 +0100

    remove duplicates

--- packages/embodi/src/exports/lib.ts
+++ packages/embodi/src/exports/lib.ts
@@ -2,4 +2,9 @@ export { createDevServer, createPreviewServer } from '../core/dev.js';
 export { generate } from '../core/build.js';
 export { defineConfig } from '../core/app/config.js';
 
-export type { RenderHookEvent, RenderHook } from '../core/definitions/types.js';
+export type {
+	RenderHookEvent,
+	RenderHook,
+	LoadAction,
+	LoadEvent
+} from '../core/definitions/types.js';
