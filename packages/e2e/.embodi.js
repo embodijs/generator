e85807c1e7f19b3f6eb3997a1317ba2eb605278e
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-11-10 23:19:18 +0100

    add package for testing e2e
    move playwright to root

---
+++ packages/e2e/.embodi.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "embodi";
+
+export default defineConfig({
+  publicDir: "./public",
+  layoutDir: "./__layout",
+  source: "/content",
+});
---
+++ packages/e2e/__layout/Home.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css";
+
+    let { data, children } = $props();
+</script>
+
+<main>
+    <h1>{data.title}</h1>
+    {@render children?.()}
+</main>
