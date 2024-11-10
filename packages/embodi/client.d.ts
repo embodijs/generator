ec874711192b410bec81f6234d9e305ddb947d75
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-11-10 22:30:51 +0100

    export types for collections and stores

---
+++ packages/embodi/client.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="./dist/core/vite/virtual-modules/embodi/collections.d.ts" />
+/// <reference path="./dist/core/vite/virtual-modules/embodi/stores.d.ts" />
--- packages/embodi/src/core/vite/virtual-modules/embodi/collections.d.ts
+++ packages/embodi/src/core/vite/virtual-modules/embodi/collections.d.ts
@@ -1,3 +1,8 @@
+declare module '$embodi/collections?*' {
+	export const meta: Array<import('../../utils/collections.ts').CollectionMeta>;
+	export const collections: Array<any>;
+}
+
 declare module '$embodi/collections' {
 	export const meta: Array<import('../../utils/collections.ts').CollectionMeta>;
 	export const collections: Array<any>;
