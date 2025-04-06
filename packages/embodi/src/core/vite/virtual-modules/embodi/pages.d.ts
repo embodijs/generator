34b989783e21f6e4f0ab063871f38be5fc642d06
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-04-06 17:31:09 +0200

    match page types with new page structure

--- packages/embodi/src/core/vite/virtual-modules/embodi/pages.d.ts
+++ packages/embodi/src/core/vite/virtual-modules/embodi/pages.d.ts
@@ -1,4 +1,8 @@
 /// <reference types="svelte" />
+declare module 'virtual-page:*' {
+  export default import('../../../definitions/types.d.ts').PageData
+}
+
 declare module '$embodi/pages' {
 	export const pages: Record<string, import('../../../definitions/types.d.ts').PageImportFunction>;
 	export const routes: Record<string, string>;
