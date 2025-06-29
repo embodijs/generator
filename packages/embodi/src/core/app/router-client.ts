5b2ca975f398b2a2080b3b28796c450146d591e7
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-06-29 22:57:17 +0200

    implement function to get page import with validation

--- packages/embodi/src/core/app/router-client.ts
+++ packages/embodi/src/core/app/router-client.ts
@@ -1,16 +1,24 @@
 import { addTrailingSlash } from './utils/paths.js';
 import { routes, pages } from '$embodi/pages';
 import { data as globalData } from '$embodi/data';
+import type { PageData } from 'core/definitions/types.js';
 
 const convertUrlToPath = async (url: string) => {
 	return routes[url];
 };
 
+const getPage = (url: string): (() => Promise<{ default: PageData }>) => {
+	if (Object.hasOwnProperty.call(pages, url) && typeof pages[url] === 'function') {
+		return pages[url];
+	}
+
+	throw new Error('Pages does not exist');
+};
+
 const getPageFromUrl = async (_url: string | URL) => {
 	const url = typeof _url === 'string' ? addTrailingSlash(_url) : _url.pathname;
-	if (!Object.hasOwnProperty.call(pages, url)) return;
-	const pageImportFu = pages[url];
-	if (typeof pageImportFu !== 'function') return;
+
+	const pageImportFu = getPage(url);
 	const controller = new AbortController();
 	const loadData = async (url: string) =>
 		(await fetch(`${url}data.json`, { signal: controller.signal }))?.json();
