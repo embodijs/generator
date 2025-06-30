1fa1414f95b39c4da0a7f9e9f7ed2eb0f4a54d29
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-07-01 01:19:00 +0200

    extract html plugin from embodi core

---
+++ .changeset/free-bags-grin.md
@@ -0,0 +1,5 @@
+---
+'@embodi/html': minor
+---
+
+Extract html parse plugin from embodi
--- plugins/html/src/plugin.ts
+++ plugins/html/src/plugin.ts
@@ -2,9 +2,9 @@ import type { Plugin } from 'vite';
 import { createContentParserPlugin } from '../utils/parser-plugin.js';
 
 export function embodiHtml(): Plugin {
-	return createContentParserPlugin({
-		name: 'vite-embodi-html',
-		fileType: 'html',
-		convertContent: (content) => content
-	});
+  return createContentParserPlugin({
+    name: 'vite-embodi-html',
+    fileType: 'html',
+    convertContent: (content) => content
+  });
 }
