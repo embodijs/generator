92e784f04294f03e406e0a1816831f49dc05980b
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-08-03 19:25:42 +0200

    simplify test

--- packages/e2e/tests/simple.test.ts
+++ packages/e2e/tests/simple.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('render components with a basic layout', async ({ page }) => {
-  await page.goto('/second/simple/');
+  await page.goto('/simple/');
   await expect(page.getByRole('heading', { name: 'Simple Site' })).toBeVisible();
 });
