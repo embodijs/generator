44f9f034880516a04e07f28e063baf3b2bfe90bc
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-08-29 00:58:57 +0200

    fix layout loading failure

---
+++ .changeset/heavy-pets-cheer.md
@@ -0,0 +1,5 @@
+---
+'embodi': patch
+---
+
+fix: reload file produce layout loading. Fix by handle sreachQuery of path different and set a clear type
--- packages/embodi/src/core/vite/plugins/templates.ts
+++ packages/embodi/src/core/vite/plugins/templates.ts
@@ -1,25 +1,24 @@
 import { type Plugin } from 'vite';
 import { loadConfig, type EmbodiConfig } from '../utils/config.js';
-import { prepareIdValidator, resolvePipe } from '../utils/virtuals.js';
-import { existsSync } from 'fs';}
-import { prepareComponentLoad } from '../utils/template.js';
+import { prepareIdValidator } from '../utils/virtuals.js';
+import { existsSync } from 'fs';
 import assert from 'assert';
 import { resolve } from 'node:path';
 import { join, extname } from 'node:path/posix';
 
-
 const convertTo = (path: string) => {
-  const ext = extname(path,);
-  if (ext === '') {
-    throw new Error('Invalid layout path: Path need to include a file type');
-  }
-  return path.replace(ext, `.js?${ext}`);
+	const ext = extname(path);
+	if (ext === '') {
+		throw new Error('Invalid layout path: Path need to include a file type');
+	}
+	return path.replace(ext, `.js?ext=${ext}`);
 };
 
 const convertFrom = (path: string) => {
-  const [_path, type] = path.split('?');
-  const ext = extname(_path);
-  return _path.replace(ext, type)
+	const [_path, searchQuery] = path.split('?');
+	const originalFileType = searchQuery?.split('=')[1];
+	const ext = extname(_path);
+	return _path.replace(ext, originalFileType);
 };
 
 export const templatePlugin = (): Plugin => {
@@ -34,32 +33,32 @@ export const templatePlugin = (): Plugin => {
 		},
 		resolveId(id) {
 			const resolvedId = layoutValidator.resolve(id);
-			if(resolvedId) {
-			  return convertTo(resolvedId);
+			if (resolvedId) {
+				return convertTo(resolvedId);
 			}
 			return null;
 		},
 		async load(id, options) {
 			if (layoutValidator.load(id)) {
 				assert(projectConfig);
-        const path = convertFrom(layoutValidator.getPath(id));
+				const path = convertFrom(layoutValidator.getPath(id));
 				const layoutRoot = projectConfig.inputDirs.layout;
 				const layoutPath = join('#root', layoutRoot, path);
 
 				const snippet = `export { default as Layout} from '${layoutPath}';`;
-        if (options?.ssr === true) {
-         	const layoutExtendsPathJS = resolve(layoutRoot, path + '.js');
-          const layoutExtendsPathTS = resolve(layoutRoot, path + '.ts');
-          if (existsSync(layoutExtendsPathJS) || existsSync(layoutExtendsPathTS)) {
-            return `${snippet}\n
+				if (options?.ssr === true) {
+					const layoutExtendsPathJS = resolve(layoutRoot, path + '.js');
+					const layoutExtendsPathTS = resolve(layoutRoot, path + '.ts');
+					if (existsSync(layoutExtendsPathJS) || existsSync(layoutExtendsPathTS)) {
+						return `${snippet}\n
   				export * as layoutActions from '${layoutPath}.js';
-  				`
-          } else {
-            return `${snippet}\n
+  				`;
+					} else {
+						return `${snippet}\n
   				export const layoutActions = {};
-  				`
-          }
-        }
+  				`;
+					}
+				}
 				return snippet;
 			}
 		}
